32d72079970457fe7cb3b8d9e43e610cd8f5949e
Daniel <wyimaginowany12@gmail.com>
2016-11-12 16:31:12 +0100

    Header small changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
       <div class="nav-left">
         <a class="nav-item is-brand" href="{{ url('/') }}">
-          <h1 class="title">&nbspShort.URL</h1>
+          <h1 class="title">&nbspshortIT</h1>
         </a>
           <a class="nav-item is-tab" href="{{ url('/') }}">
         <i class="fa fa-home"></i>&nbspHome
