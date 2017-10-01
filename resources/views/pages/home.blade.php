afd21d0796ad83fc9bcdd4a54b2d5d15a2ed5c3f
nybbom <dlesniak781@gmail.com>
2017-10-01 21:19:52 +0200

    Transform html form to LaravelCollective.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,42 +6,41 @@
 	<div class="hero-body">
 		<div class="container">
 			<div class="columns">
-				<div class="column is-two-thirds">
+				<div class="column is-three-quarters">
 					<h1 class="title">Enter a long URL:</h1>
 					<form action="{{ @action('ShortenController@store') }}" method="post" class="field has-addons">
 						{{--  Csrf protection  --}}
-						{{ csrf_field() }}
-						<p class="control">
-							{{-- Protocol Select --}}
-							<span class="select is-large">
-								<select name="protocol_select" title="Select protocol">
-									<option value="http://" style="color: red">http://</option>
-									<option value="https://" style="color: green" selected>https://</option>
-								</select>
-							</span>
+						{{ Form::token() }}
+						<p class="control is-expanded">
+							{{-- URL --}}
+							{!! Form::url('url', old('url'),
+								['class' => 'input is-large',
+								'autocomplete' => 'off',
+								'autofocus',
+								'placeholder' => route('home')]
+								) !!}
 						</p>
-						<p class="control">
-							{{--  Url Input (without protocol)  --}}
-							<input class="input is-large is-expanded"
-								placeholder="{{ str_replace(['http://', 'https://'], '', route('home')) }}" name="url" autocomplete="off" autofocus>
-						</p>
-						{{--  Custom Alias Input  --}}
+						{{--  Custom Alias Button  --}}
 						<p class="control is-not-visible" id="custom-alias-prefix">
 							<a class="button is-static is-large">|</a>
 						</p>
+						{{-- Custom alias input --}}
 						<p class="control" id="custom-alias-control">
-							<input name="custom_alias" class="input is-large is-not-visible" placeholder="[a-z] [1-9] [-,_]" id="custom_alias_input">
+							{!! Form::text('slug', '',
+							['class' => 'input is-large is-not-visible',
+							'placeholder' => '[a-z] [1-9] [-,_]',
+							'id' => 'custom_alias_input',
+							])
+							!!}
 							<button type="button" class="button is-info is-large" id="custom-alias-button">CUSTOM ALIAS</button>
 						</p>
-						{{--  is_private Hidden  --}}
-						<input type="hidden" name="is_private" value="false">
 						<p class="control">
-							{{--  Is Private button  --}}
+							{!! Form::hidden('is_private', 'false') !!}
 							<button type="button" class="button is-info is-large" id="is-private"><i class="fa fa-user-secret"></i></button>
 						</p>
 						{{--  Submit  --}}
 						<p class="control">
-                            <input type="submit" class="button is-info is-large" value="SHORTEN">
+							{!! Form::submit('SHORTEN', ['class' => 'button is-info is-large']) !!}
 						</p>
 					</form>
 
@@ -63,7 +62,9 @@
 </div>
 
 {{-- My Shortens --}}
-@include('partials.my-shortens', $myShortens)
+@if(isset($myShortens))
+	@include('partials.my-shortens', $myShortens)
+@endif
 
 {{-- Newest Shortens --}}
 @include('partials.newest-shortens', $newestShortens)
