6c5bfe69d489edd08e297c8225f32e36ce1e0c5d
nybbom <dlesniak781@gmail.com>
2017-10-03 21:01:40 +0200

    Remove protocol.

--- public/js/app.js
+++ public/js/app.js
@@ -5,16 +5,7 @@ $('#is-private').on('click', function(e) {
     $('input[name=is_private]').val(is_private)
     $(this).toggleClass('is-active')
     $('#is_private_info').toggle()
-
 });
-
-$('select[name=protocol_select]').on('change', function() {
-    updateModel($('input[name=url]'), $(this), $('input[name=url_with_protocol]'))
-})
-
-$('input[name=url]').on('keyup', function() {
-    updateModel($(this), $('select[name=protocol_select]'), $('input[name=url_with_protocol]'))
-})
 
 $('#custom-alias-button').on('click', function() {
     $(this).hide()
@@ -23,17 +14,6 @@ $('#custom-alias-button').on('click', function() {
     $('#custom_alias_info').removeClass('is-not-visible')
     $('input[name=custom_alias]').focus()
 })
-
-function updateModel(urlInput, protocolSelect, urlWithProtocolHidden) {
-    let urlInputValue = urlInput.val()
-    let protocolSelectValue = protocolSelect.val()
-    let urlWithoutProtocol = urlInputValue.replace('https://', '')
-    urlWithoutProtocol = urlWithoutProtocol.replace('http://', '')
-
-    urlInput.val(urlWithoutProtocol)
-
-    urlWithProtocolHidden.val(protocolSelectValue + urlInputValue)
-}
 
 $('.refererAnchor').each(function () {
     let anchorText = $(this).text()
